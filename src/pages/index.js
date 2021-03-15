aa9090e681e2f7122db14ca88eaa4d94bb90c2c9
Alec Pagliarussi <alec.paglia@gmail.com>
2021-03-14 20:14:36 -0400

    attempt to improve metatags

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,32 @@ import App from "../components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../style/main.scss";
 import headData from "./../data/headData.json";
+import Thumbnail from "./../images/Alec_Pagliarussi_Profile_picture.jpg";
 
 export default () => {
   const { title, lang, description } = headData;
+
+  const socialLinks = {
+    linkedin: "https://www.linkedin.com/in/alecbp/",
+    github: "https://github.com/AlecBp",
+  };
 
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
         <title>{title}</title>
-        <html lang={lang} />
         <meta name="description" content={description} />
+        <meta name="image" content={Thumbnail} />
+        <html lang={lang} />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content={socialLinks} />
+        <meta name="twitter:site" content={"@AlecPaglia"} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image:src" content={Thumbnail} />
       </Helmet>
       <App />
     </>
